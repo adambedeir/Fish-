73847c01f2184adf4bdf9ca90b7cb5ebb09cc33f
adambedeir <adambedeir@gmail.com>
2020-07-13 17:34:18 +0000

    edit the function name

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ size=(width,hieght)=(screen_info.current_w,screen_info.current_h)
 clock=pygame.time.clock
 screen=pygame.display.set_mode(size)
 color= (0,255,0)
-def function():
+def main():
   clock.tick(60)
   screen.fill
   pygame.display.flip()
 if __name__=='__main__':
-  function()
+  main()
