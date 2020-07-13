5446766fb19994b0af3044e40c5f7394b8ef76af
adambedeir <adambedeir@gmail.com>
2020-07-13 17:33:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+
+screen_info=pygame.display.Info()
+print(screen_info)
+size=(width,hieght)=(screen_info.current_w,screen_info.current_h)
+clock=pygame.time.clock
+screen=pygame.display.set_mode(size)
+color= (0,255,0)
+def function():
+  clock.tick(60)
+  screen.fill
+  pygame.display.flip()
+if __name__=='__main__':
+  function()
